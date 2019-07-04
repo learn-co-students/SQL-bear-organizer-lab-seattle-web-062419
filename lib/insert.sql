197f21334f073f4d2fcd941d1d364124231965fe
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-07-03 17:54:11 -0700

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive)
+VALUES (
+    "Mr. Chocolate", 26, "male", "black", "sanguine", true);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive)
+VALUES (
+    "Rowdy", 10, "male", "brown", "choleric", true);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive)
+VALUES (
+    "Tabintha", 7, "female", "black", "philegmatic", true);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive)
+VALUES (
+    "Sergeant Borwn", 18, "male", "brown", "melancholic", false);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive)
+VALUES (
+    "Melissa", 12, "female", "black", "sanguine", true);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive)
+VALUES (
+    "Grinch", 20, "male", "brown", "melancholic", false);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive)
+VALUES (
+    "Wendy", 15, "female", "brown", "philegmatic", false);
+
+INSERT INTO bears (
+    name, age, gender, color, temperament, alive)
+VALUES (
+    NULL, 19, "male", "black", "choleric", false);
